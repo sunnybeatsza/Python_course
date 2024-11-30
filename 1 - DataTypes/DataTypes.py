c7fbbc0acf25f3c1abd66049e817a3876971754c
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2024-11-30 05:07:42 +0200

    starter code

---
+++ 1 - DataTypes/DataTypes.py
@@ -0,0 +1,22 @@
+#This is an example of a integer data type
+integer_number = 1
+integer_number2 = 20
+integer_number3 = 45
+
+
+#This is an example of floating point data type
+floating_number = 2.0
+floating_number2 = 5.52
+floating_number3 = 25.65
+
+
+#This is an example of a string data type
+string1 = "Tumisho"
+string2 = "Money"
+string3 = "Charaters in a string"
+
+
+#This is an example of a list data type
+floating_number = 2.0
+floating_number2 = 5.52
+floating_number3 = 25.65
