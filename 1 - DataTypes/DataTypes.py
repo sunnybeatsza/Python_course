634db2fb52fe3447cddd707cda21df58fa4fb272
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2024-11-30 05:29:02 +0200

    changes to the code

--- 1 - DataTypes/DataTypes.py
+++ 1 - DataTypes/DataTypes.py
@@ -1,30 +1,37 @@
 #This is an example of a integer data type
-integer_number = 1
-integer_number2 = 20
-integer_number3 = 45
+1
+20
+45
 
 
 #This is an example of floating point data type
-floating_number = 2.0
-floating_number2 = 5.52
-floating_number3 = 25.65
+2.0
+5.52
+25.65
 
 
 #This is an example of a string data type
-string1 = "Tumisho"
-string2 = "Money"
-string3 = "Charaters in a string"
+"Tumisho"
+"Money"
+"Charaters in a string"
 
 
 #This is an example of a list data type
 #Note - A list can have multiple data types store in a collection
-list1 = ["Item1", 2, 100.50]
-list2 = ["Item1", 2, 100.50]
-list3 = ["Item1", 2, 100.50]
+["Item1", 2, 100.50]
+["Item1", 2, 100.50]
+["Item1", 2, 100.50]
+
+
+#This is an example of a dictionary data type
+{"Key":"Value", "balance":1500, "hello":"molweni"}
+{"Key":"Value", "balance":1500, "hello":"molweni"}
+{"Key":"Value", "balance":1500, "hello":"molweni"}
+
 
 #This is an example of a tuple data type
 #Note - A tuple can have multiple data types stored in a collection
-tuple1 = (10,"Aang",100.50)
-tuple2 = (45,"Sokka","")
-
+(10,"Aang",100.50)
+(45,"Sokka",True)
+(False,"Cars", 500)
 
