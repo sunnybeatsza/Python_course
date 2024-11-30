1cda187b51d772b0337c7a96ecdf73f23babec90
momakgejhb024 <momakgejhb024@student.wethinkcode.co.za>
2024-11-30 05:36:03 +0200

    changes

--- 1 - DataTypes/DataTypes.py
+++ 1 - DataTypes/DataTypes.py
@@ -1,37 +1,41 @@
-#This is an example of a integer data type
+#These are examples of integer data types
 1
 20
 45
 
 
-#This is an example of floating point data type
+#These are examples of floating data types
 2.0
 5.52
 25.65
 
 
-#This is an example of a string data type
+#These are examples of string data types
 "Tumisho"
 "Money"
 "Charaters in a string"
 
 
-#This is an example of a list data type
+#These are examples of list data types
 #Note - A list can have multiple data types store in a collection
 ["Item1", 2, 100.50]
 ["Item1", 2, 100.50]
 ["Item1", 2, 100.50]
 
 
-#This is an example of a dictionary data type
+#These are examples of dictionary data types
 {"Key":"Value", "balance":1500, "hello":"molweni"}
 {"Key":"Value", "balance":1500, "hello":"molweni"}
 {"Key":"Value", "balance":1500, "hello":"molweni"}
 
 
-#This is an example of a tuple data type
+#These are examples of tuple data types
 #Note - A tuple can have multiple data types stored in a collection
 (10,"Aang",100.50)
 (45,"Sokka",True)
 (False,"Cars", 500)
 
+
+#These are examples of of boolean data types
+False
+True
